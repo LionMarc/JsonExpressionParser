27fe9b9a16b4020c45aa1abbc7cf261b19aee731
Marc LION <lionmarc@free.fr>
2017-07-05 13:47:17 +0200

    Add operations + and -

--- JsonExpressionParser.Tests/JsonExpressionParserTest.cs
+++ JsonExpressionParser.Tests/JsonExpressionParserTest.cs
@@ -130,6 +130,66 @@
             Assert.AreEqual(new DateTime(2017, 5, 8, 2, 0, 0, DateTimeKind.Utc), result);
         }
 
+        [TestMethod]
+        public void Should_sum_the_double_field_with_the_numeric_constant_when_constant_is_at_right()
+        {
+            var jsonExpressionParser = new JsonExpressionParser<JsonExpressionParserContext>();
+
+            var expression = "$.realAskPrice + 4.0";
+            var func = jsonExpressionParser.CreateFuncFromExpression<double>(expression);
+
+            var context = new JsonExpressionParserContext(this.inputData);
+
+            var result = func(context);
+
+            Assert.AreEqual(127.456, result, 1E-10);
+        }
+
+        [TestMethod]
+        public void Should_sum_the_double_field_with_the_numeric_constant_when_constant_is_at_left()
+        {
+            var jsonExpressionParser = new JsonExpressionParser<JsonExpressionParserContext>();
+
+            var expression = "5.5 + $.realAskPrice";
+            var func = jsonExpressionParser.CreateFuncFromExpression<double>(expression);
+
+            var context = new JsonExpressionParserContext(this.inputData);
+
+            var result = func(context);
+
+            Assert.AreEqual(128.956, result, 1E-10);
+        }
+
+        [TestMethod]
+        public void Should_substract_the_constant_value_from_the_double_field()
+        {
+            var jsonExpressionParser = new JsonExpressionParser<JsonExpressionParserContext>();
+
+            var expression = "$.realAskPrice - 4.0";
+            var func = jsonExpressionParser.CreateFuncFromExpression<double>(expression);
+
+            var context = new JsonExpressionParserContext(this.inputData);
+
+            var result = func(context);
+
+            Assert.AreEqual(119.456, result, 1E-10);
+        }
+
+        [TestMethod]
+        public void Should_substract_the_double_field_from_the_constant_value()
+        {
+            var jsonExpressionParser = new JsonExpressionParser<JsonExpressionParserContext>();
+
+            var expression = "142 - $.realAskPrice";
+            var func = jsonExpressionParser.CreateFuncFromExpression<double>(expression);
+
+            var context = new JsonExpressionParserContext(this.inputData);
+
+            var result = func(context);
+
+            Assert.AreEqual(18.544, result, 1E-10);
+        }
+
         #endregion
     }
 }
--- JsonExpressionParser/JsonExpressionParser.cs
+++ JsonExpressionParser/JsonExpressionParser.cs
@@ -1,6 +1,7 @@
 ﻿namespace JsonExpressionParser
 {
     using System;
+    using System.Globalization;
     using System.Linq.Expressions;
     using System.Reflection;
     using Newtonsoft.Json.Linq;
@@ -11,7 +12,9 @@
         #region Fields
 
         private readonly ParameterExpression currentContextExpression = Expression.Parameter(typeof(TContext));
-        private readonly Parser<Expression> jsonInputField;
+        private readonly Parser<Expression> Expr;
+
+        private readonly Parser<Expression> term;
 
         #endregion
 
@@ -19,8 +22,22 @@
 
         public JsonExpressionParser()
         {
-            this.jsonInputField = Parse.Regex(@"\$(\.[a-zA-Z][a-zA-Z0-9]*)*")
+            var number = Parse.DecimalInvariant
+                .Select(x => Expression.Constant(double.Parse(x, CultureInfo.InvariantCulture)));
+            var jsonInputField = Parse.Regex(@"\$(\.[a-zA-Z][a-zA-Z0-9]*)*")
                 .Select(s => this.GenerateExpressionForJsonField(s));
+
+            this.term = (from lparen in Parse.Char('(')
+                         from expr in Parse.Ref(() => Expr)
+                         from rparen in Parse.Char(')')
+                         select expr).Named("expression")
+                         .XOr(number)
+                         .XOr(jsonInputField);
+
+            this.Expr = Parse.ChainOperator(
+                Operator("+", ExpressionType.AddChecked).Or(Operator("-", ExpressionType.SubtractChecked)),
+                this.term,
+                MakeBinary);
         }
 
         #endregion
@@ -29,7 +46,7 @@
 
         public Func<TContext, TResultType> CreateFuncFromExpression<TResultType>(string expression)
         {
-            return this.jsonInputField.End()
+            return this.Expr.End()
                 .Select(body => Expression.Lambda<Func<TContext, TResultType>>(
                     Expression.Convert(body, typeof(TResultType)),
                     this.currentContextExpression))
@@ -40,6 +57,27 @@
         #endregion
 
         #region Helper Methods
+
+        private static Expression MakeBinary(ExpressionType type, Expression left, Expression right)
+        {
+            if (left.Type != typeof(object))
+            {
+                return Expression.MakeBinary(
+                    type,
+                    left,
+                    Expression.Convert(right, left.Type));
+            }
+
+            return Expression.MakeBinary(
+                type,
+                Expression.Convert(left, right.Type),
+                right);
+        }
+
+        private static Parser<ExpressionType> Operator(string op, ExpressionType opType)
+        {
+            return Parse.String(op).Token().Return(opType);
+        }
 
         private static object GetJsonField(string jsonPath, TContext context)
         {
